ceb56f5063b7cf3696d6fc63b07c9a73da2d3768
psetraki <psetraki@alumnos.unex.es>
2022-12-04 12:56:50 +0100

    arreglado, estaba mal escrito el paquete del controlador

--- src/main/java/es/unex/cum/mdp/sesion10/MainCalculadora.java
+++ src/main/java/es/unex/cum/mdp/sesion10/MainCalculadora.java
@@ -2,20 +2,24 @@ package main.java.es.unex.cum.mdp.sesion10;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainCalculadora extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("ej7Calculadora.fxml")); // Ponemos el fichero bfxml creado
+            Scene scene = new Scene(root, 400, 400);
+            scene.getStylesheets().add(getClass().getResource("ej7Estilos.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 
     public static void main(String[] args) {
         launch(args);
