97ea8e5dcfb689f1965209801698135eecff5001
Pablo <pablosetra@icloud.com>
2022-12-04 12:34:39 +0100

    prueba

---
+++ src/main/java/es/unex/cum/mdp/sesion10/Calculadora.java
@@ -0,0 +1,5 @@
+package main.java.es.unex.cum.mdp.sesion10;
+
+public class Calculadora {
+	
+}
---
+++ src/main/java/es/unex/cum/mdp/sesion10/CalculadoraController.java
@@ -0,0 +1,183 @@
+package main.java.es.unex.cum.mdp.sesion10;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CalculadoraController implements Initializable {
+
+	 @FXML
+	    private Button boton0;
+
+	    @FXML
+	    private Button boton1;
+
+	    @FXML
+	    private Button boton2;
+
+	    @FXML
+	    private Button boton3;
+
+	    @FXML
+	    private Button boton4;
+
+	    @FXML
+	    private Button boton5;
+
+	    @FXML
+	    private Button boton6;
+
+	    @FXML
+	    private Button boton7;
+
+	    @FXML
+	    private Button boton8;
+
+	    @FXML
+	    private Button boton9;
+
+	    @FXML
+	    private Button botonClear;
+
+	    @FXML
+	    private Button botonClearEntry;
+
+	    @FXML
+	    private Button botonDivision;
+
+	    @FXML
+	    private Button botonIgual;
+
+	    @FXML
+	    private Button botonMas;
+
+	    @FXML
+	    private Button botonMasMenos;
+
+	    @FXML
+	    private Button botonMenos;
+
+	    @FXML
+	    private Button botonMsg;
+
+	    @FXML
+	    private Button botonMultiplicacion;
+
+	    @FXML
+	    private Button botonPunto;
+
+	    @FXML
+	    private TextField text1;
+
+	    @FXML
+	    void ActionCambiarSigno(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void ActionClear(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void ActionClearEntry(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void ActionMsg(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action0(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action1(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action2(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action3(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action4(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action5(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action6(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action7(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action8(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void action9(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void actionDividir(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void actionIgual(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void actionMas(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void actionMenos(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void actionMultiplicar(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void actionPunto(ActionEvent event) {
+
+	    }
+	    
+	    @Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+}
