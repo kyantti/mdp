e92852f13e5bacca935f6440b92d8ef3c8566fbf
psetraki <psetraki@alumnos.unex.es>
2022-12-12 10:54:10 +0100

    register

--- src/main/java/es/unex/cum/mdp/ef3/controller/RegisterController.java
+++ src/main/java/es/unex/cum/mdp/ef3/controller/RegisterController.java
@@ -4,25 +4,37 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import main.java.es.unex.cum.mdp.ef3.App;
 
-public class WelcomeController {
+public class RegisterController {
+    @FXML
+    private TextField passwordTextField;
 
     @FXML
-    private PasswordField password;
+    private TextField specialCodeTextfield;
 
     @FXML
     private TextField userTextField;
 
     @FXML
     void createAccount(ActionEvent event) throws IOException {
-        App.setRoot("view/register");
+        App.setRoot("view/welcome");
     }
 
     @FXML
-    void login(ActionEvent event) throws IOException {
-        App.setRoot("view/userMenu");
+    void setPassword(ActionEvent event) {
+
     }
+
+    @FXML
+    void setSpecialCode(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setUser(ActionEvent event) {
+
+    }
+
 }
